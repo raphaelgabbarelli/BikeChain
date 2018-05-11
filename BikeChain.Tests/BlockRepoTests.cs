b1f7a2152271e3c7e06548261bd2ba5a4f8f6bcb
Raphael Gabbarelli <raphael@gabbarelli.it>
2018-05-11 23:35:57 +0200

    a bit convoluted way to use hex strings and test them

--- BikeChain.Tests/BlockRepoTests.cs
+++ BikeChain.Tests/BlockRepoTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BikeChain.Tests.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -14,24 +15,22 @@ namespace BikeChain.Tests
         {
             
             DateTime timestamp = new DateTime(1998, 6, 7);
-            byte[] prevHash = StringToByteArray("916ea4dc3717b0c46156ed9382e9dac329212d3d1bdd30516bdabef670798feb");
-            byte[] thisHash = StringToByteArray("18f34ff0ff9b8a8ba44d2f73c26e3ba179acf62225ab87cf3afb86dcb95b34d4");
+            string prevHash = "916ea4dc3717b0c46156ed9382e9dac329212d3d1bdd30516bdabef670798feb";
+            byte[] prevHashBytes = Transformations.StringToByteArray(prevHash);
+            string thisHash = "18f34ff0ff9b8a8ba44d2f73c26e3ba179acf62225ab87cf3afb86dcb95b34d4";
+            byte[] thisHashBytes = Transformations.StringToByteArray(thisHash);
 
             BlockRepository systemUnderTest = new BlockRepository();
             Block genesis = systemUnderTest.CreateGenesisBlock();
             string representation = genesis.ToString();
-
+            
             Assert.Contains(timestamp.Ticks.ToString(), representation);
-            Assert.Equal(prevHash, genesis.PreviousHash);
-            Assert.Equal(thisHash, genesis.Hash);
+            Assert.Equal(prevHashBytes, genesis.PreviousHash);
+            Assert.Contains(prevHash, representation);
+            Assert.Equal(thisHashBytes, genesis.Hash);
+            Assert.Contains(thisHash, representation);
         }
 
-        static byte[] StringToByteArray(string hex)
-        {
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
-        }
+        
     }
 }
---
+++ BikeChain.Tests/Utils/Transformations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BikeChain.Tests.Utils
+{
+    public static class Transformations
+    {
+        /// <summary>
+        /// Takes a hex representation of a byte array, and converts it to an actual byte array
+        /// </summary>
+        /// <param name="hex">string that represent the hexadecimal values of a byte array</param>
+        /// <returns></returns>
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static string ByteArrayToHexString(byte[] array)
+        {
+            return BitConverter.ToString(array).Replace("-", "").ToLower();
+        }
+    }
+}
